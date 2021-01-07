44cf4b5d5bb62b1e3dcb426edf7abfa8d597783a
mokyoungg <mokyoungg@gmail.com>
2021-01-07 22:36:40 +0900

    [ADD] Intro 컴포넌트 제작
    - node sass 설치(4.14 버전)
    - reset.scss 파일 추가
    - gsap 연습을 위한 Practice 컴포넌트 제작
    - Intro 컴포넌트 제작
    - Intro 컴포넌트를 위한 Intro.scss 제작
    - Intro 컴포넌트에서 활용할 이미지 추가
    - gsap 을 활용한 애니메이션 기능 추가

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import "./Intro.scss";
+import { gsap } from "gsap";
+
+const Intro = () => {
+  useEffect(() => {
+    gsap.to(".left", {
+      duration: 2,
+      delay: 0.8,
+      width: "50%",
+      ease: "Power2.easeInOUt",
+    });
+    gsap.to(".right", {
+      duration: 2,
+      delay: 0.6,
+      width: "50%",
+      ease: "Power3.easeInOut",
+    });
+    gsap.to(".img1", {
+      duration: 2,
+      delay: 1.5,
+      width: "530px",
+      ease: "Power2.easeInOut",
+    });
+    gsap.from(".text h1", {
+      duration: 2,
+      delay: 0.6,
+      x: 1000,
+      ease: "Circ.easeInOut",
+    });
+    gsap.from(".text p", {
+      duration: 2,
+      delay: 0.6,
+      x: 1000,
+      ease: "Circ.easeInOut",
+    });
+  }, []);
+
+  return (
+    <div className="body">
+      <div className="left"></div>
+      <div className="right"></div>
+      <div className="content">
+        <div className="img-wrapper">
+          <div className="img1"></div>
+        </div>
+      </div>
+      <div className="text">
+        <p>
+          The
+          <br /> Masetro
+        </p>
+        <h1>
+          Zinedine
+          <br /> Zidane
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Intro from "./components/intro/Intro";
 
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Intro />, document.getElementById("root"));
